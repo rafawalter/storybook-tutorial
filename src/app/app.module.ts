9275a182ef4453de934ebc2b3ce9e9a398f22fd3
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2022-10-18 12:39:57 -0300

    Wire in data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {TaskModule} from "./components/task.module";
+import {NgxsModule} from "@ngxs/store";
+import {environment} from "../environments/environment";
+import {NgxsReduxDevtoolsPluginModule} from "@ngxs/devtools-plugin";
+import {NgxsLoggerPluginModule} from "@ngxs/logger-plugin";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    TaskModule,
+    NgxsModule.forRoot([], {
+      developmentMode: !environment.production,
+    }),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    NgxsLoggerPluginModule.forRoot({
+      disabled: environment.production,
+    })
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/pure-task-list.stories.ts
+++ src/app/components/pure-task-list.stories.ts
@@ -1,19 +1,22 @@
-import {TaskListComponent} from "./task-list.component";
+import {PureTaskListComponent} from "./pure-task-list.component";
 import {TaskComponent} from "./task.component";
 import {componentWrapperDecorator, Meta, moduleMetadata, Story} from "@storybook/angular";
 import {CommonModule} from "@angular/common";
 import * as TaskStories from "./task.stories";
 
 export default {
-  component: TaskListComponent,
+  component: PureTaskListComponent,
   decorators: [
     moduleMetadata({
-      declarations: [TaskListComponent, TaskComponent],
+      //👇 Imports both components to allow component composition with storybook
+      declarations: [PureTaskListComponent, TaskComponent],
       imports: [CommonModule],
     }),
+    //👇 Wraps our stories with a decorator
     componentWrapperDecorator(story => `<div style="margin: 3em">${story}</div>`),
+
   ],
-  title: 'TaskList',
+  title: 'PureTaskList',
 } as Meta;
 
 const Template: Story = args => ({
@@ -38,6 +41,8 @@ Default.args = {
 
 export const WithPinnedTasks = Template.bind({});
 WithPinnedTasks.args = {
+  // Shaping the stories through args composition.
+  // Inherited data coming from the Default story.
   tasks: [
     ...Default.args['tasks'].slice(0,5),
     { id: '6', title: ' Task 6 (pinned)', state: 'TASK_PINNED'},
@@ -52,6 +57,8 @@ Loading.args = {
 
 export const Empty = Template.bind({});
 Empty.args = {
+  // Shaping the stories through args composition.
+  // Inherited data coming from the Loading story.
   ...Loading.args,
   loading: false,
 }
